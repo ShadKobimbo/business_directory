497f183cad65e8138c446650aa59599bc293fa20
ShadKobimbo <shadkobimbo@outlook.com>
2018-11-01 11:03:55 +0300

    Add files via upload

---
+++ css/index.php
@@ -0,0 +1,275 @@
+<?php include 'header.php';?>
+    <!-- ***** Welcome Area Start ***** -->
+    <section class="dorne-welcome-area bg-img bg-overlay" style="background-image: url(img/bg-img/hero-1.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center justify-content-center">
+                <div class="col-12 col-md-10">
+                    <div class="hero-content">
+                        <h2>Finding services near you</h2>
+                    </div>
+                    <!-- Hero Search Form -->
+                    <div class="hero-search-form">
+                        <!-- Tabs -->
+                        <!-- Tabs Content -->
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-places" role="tabpanel" aria-labelledby="nav-places-tab">
+                                <h6>What are you looking for?</h6>
+                                <form action="#" method="get">
+                                    <select class="custom-select">
+                                        <option selected>Your Location</option>
+                                        <option value="1">Langata</option>
+                                        <option value="2">Kinoo</option>
+                                        <option value="3">Ruiru</option>
+                                    </select>
+                                    <select class="custom-select">
+                                        <option selected>Services</option>
+                                        <option value="1">Plumbing</option>
+                                        <option value="2">Electrical</option>
+                                        <option value="3">Laundry</option>
+                                    </select>
+                                    <select class="custom-select">
+                                        <option selected>Price Range</option>
+                                        <option value="1">$100 - $499</option>
+                                        <option value="2">$500 - $999</option>
+                                        <option value="3">$1000 - $4999</option>
+                                    </select>
+                                    <button type="submit" class="btn dorne-btn"><i class="fa fa-search pr-2" aria-hidden="true"></i> Search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero Social Btn -->
+        <div class="hero-social-btn">
+            <div class="social-title d-flex align-items-center">
+                <h6>Follow us on Social Media</h6>
+                <span></span>
+            </div>
+            <div class="social-btns">
+                <a href="#"><i class="fa fa-linkedin" aria-haspopup="true"></i></a>
+                <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-haspopup="true"></i></a>
+                <a href="#"><i class="fa fa-facebook" aria-haspopup="true"></i></a>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Welcome Area End ***** -->
+
+    <!-- ***** Catagory Area Start ***** -->
+    <section class="dorne-catagory-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="all-catagories">
+                        <div class="row">
+                            <!-- Single Catagory Area -->
+                            <div class="col-12 col-sm-6 col-md">
+                                <div class="single-catagory-area wow fadeInUpBig" data-wow-delay="0.2s">
+                                    <div class="catagory-content">
+                                        <img src="img/core-img/icon-1.png" alt="">
+                                        <a href="#">
+                                            <h6>Plumbing</h6>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Catagory Area -->
+                            <div class="col-12 col-sm-6 col-md">
+                                <div class="single-catagory-area wow fadeInUpBig" data-wow-delay="0.4s">
+                                    <div class="catagory-content">
+                                        <img src="img/core-img/icon-2.png" alt="">
+                                        <a href="#">
+                                            <h6>Electrical</h6>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Catagory Area -->
+                            <div class="col-12 col-sm-6 col-md">
+                                <div class="single-catagory-area wow fadeInUpBig" data-wow-delay="0.6s">
+                                    <div class="catagory-content">
+                                        <img src="img/core-img/icon-3.png" alt="">
+                                        <a href="#">
+                                            <h6>Laundry</h6>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Catagory Area End ***** -->
+
+    <!-- ***** About Area Start ***** -->
+    <section class="dorne-about-area section-padding-0-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="about-content text-center">
+                        <h2>Discover your city with <br><span>Dorne</span></h2>
+                        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce quis tempus elit. Sed efficitur tortor neque, vitae aliquet urna varius sit amet. Ut rhoncus, nunc nec tincidunt volutpat, ex libero.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Area End ***** -->
+
+    <!-- ***** Features Events Area Start ***** -->
+    <section class="dorne-features-events-area bg-img bg-overlay-9 section-padding-100-50" style="background-image: url(img/bg-img/hero-3.jpg)">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center">
+                        <span></span>
+                        <h4>Featured events</h4>
+                        <p>Editor’s pick</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.2s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-1.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>Jazz Concert</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.3s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-2.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>DeeJay in the house</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.4s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-3.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>Theatre Night outside</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.5s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-4.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>Wine tasting</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.6s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-5.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>New Moon Party</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="single-feature-events-area d-sm-flex align-items-center wow fadeInUpBig" data-wow-delay="0.7s">
+                        <div class="feature-events-thumb">
+                            <img src="img/bg-img/event-6.jpg" alt="">
+                            <div class="date-map-area d-flex">
+                                <a href="#">26 Nov</a>
+                                <a href="#"><img src="img/core-img/map.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="feature-events-content">
+                            <h5>Happy hour at pub</h5>
+                            <h6>Manhathan</h6>
+                            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra...</p>
+                        </div>
+                        <div class="feature-events-details-btn">
+                            <a href="#">+</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Events Area End ***** -->
+
+    <!-- ***** Clients Area Start ***** -->
+    <div class="dorne-clients-area section-padding-100">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="clients-logo d-md-flex align-items-center justify-content-around">
+                        <img src="img/clients-img/1.png" alt="">
+                        <img src="img/clients-img/2.png" alt="">
+                        <img src="img/clients-img/3.png" alt="">
+                        <img src="img/clients-img/4.png" alt="">
+                        <img src="img/clients-img/5.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Clients Area End ***** -->
+
+<?php include 'footer.php';?>
